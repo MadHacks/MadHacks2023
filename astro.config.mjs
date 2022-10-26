f659b8b17ec79bbd33c3ad64fa62532b3788722f
Nick Winans <nick.win999@gmail.com>
2022-10-25 23:17:17 -0500

    Small image upgrades

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
