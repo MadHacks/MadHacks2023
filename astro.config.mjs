8c6b86ed9eb91ff80b1129b8f7b8a8cc729a19db
Nick Winans <nick@winans.codes>
2022-10-26 15:09:27 -0500

    Final optimization to fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), compress()]
 });
