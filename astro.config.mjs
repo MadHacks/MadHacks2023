77a26e9eb8cfa7cf493f1da67aed248119f9b665
sbaumohl <sbaumohl@wisc.edu>
2024-06-20 22:15:47 -0500

    update to astro 4; attempt new deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), compress()]
+  site: 'https://www.madhacks.io',
+  integrations: [sitemap(), tailwind(), compress()]
 });
